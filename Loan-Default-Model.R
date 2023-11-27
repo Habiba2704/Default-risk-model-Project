93d166ad79ca484dd2325b52dd9618b599bd5ecc
Habiba2704 <84373596+Habiba2704@users.noreply.github.com>
2023-11-27 11:58:05 +0300

    updated variables

--- Loan-Default-Model.R
+++ Loan-Default-Model.R
@@ -10,4 +10,53 @@ Loan_Default <- read_csv("data/Loan_Default.csv",
         "0"))))
 View(Loan_Default) 
 
-## B. Exploratory Data Analysis 
+## B. Exploratory Data Analysis
+# Previewing the dataset
+dim(Loan_Default)
+# Identifying the Data Types
+sapply(Loan_Default, class)
+## MEASURES OF FREQUENCY
+### Percent
+# Identifying the number of instances that belong
+# to each class
+Loan_Default_frequency <- Loan_Default$Defaulted
+cbind(frequency = table(Loan_Default_frequency),
+      percentage = prop.table(table(Loan_Default_frequency)) * 100)
+# The results show that the dataset is highly imbalanced which could introduce a 
+# lot of bias to the resulting model; making it more biased towards predicting 
+# the majority class(non-defaults) and might not perform well in identifying instances
+# of the minority class(Defaults).
+
+## MEASURES OF CENTRAL TENDENCY 
+### Mode
+Loan_Default_Mode <- names(table(Loan_Default$Defaulted))[
+  which(table(Loan_Default$Defaulted) ==
+          max(table(Loan_Default$Defaulted)))
+]
+print(Loan_Default_Mode)
+
+### Mean and Median 
+#### For the Annual Salary variable
+mean_anSal <- mean(Loan_Default$Annual_Salary, na.rm = TRUE)
+median_anSal <- median(Loan_Default$Annual_Salary, na.rm = TRUE)
+cat("Mean Annual Salary: ", mean_anSal, "\n")
+cat("Median Annual Salary: ", median_anSal, "\n")
+#### For the Bank Balance
+mean_Bbal <- mean(Loan_Default$Bank_Balance, na.rm = TRUE)
+median_Bbal <- median(Loan_Default$Bank_Balance, na.rm = TRUE)
+cat("Mean Bank Balance: ", mean_Bbal, "\n")
+cat("Median Bank Balance: ", median_Bbal, "\n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
